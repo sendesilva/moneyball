ce94c7a7fa282f281f0777cd1b77c5fc0d47e971
Senerath de Silva <desilva.sen@gmail.com>
2018-08-25 22:59:44 +0800

    modified

--- Linear_Models.R
+++ Linear_Models.R
@@ -30,3 +30,49 @@ results %>% ggplot(aes(beta1, rss)) + geom_line() +
   geom_line(aes(beta1, rss), col=2)
 # curve shows rss min when beta1 = 0.50
 
+# lm Function provides the LSE
+fit <- lm(son ~ father, data = galton_heights)
+fit
+
+# summary function provides more info
+summary(fit)
+
+# 2.3.2 Run a linear model in R predicting the number of runs per game based on the 
+# number of bases on balls and the number of home runs. Remember to first limit your 
+# data to 1961-2001.
+library(Lahman)
+library(dslabs)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+ds_theme_set()
+# data("Teams")
+# Base on balls/game vs Home Runs/game 
+Teams <- Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(HR_per_game = HR/G, BB_per_game = BB/G, R_per_game = R/G)
+
+# lm Function provides the LSE for baseball stats
+fit <- lm(R_per_game ~ HR_per_game + BB_per_game, data = Teams)
+fit
+
+# LSE are random variables - use monte carlo to 1000 samples size 50
+B <- 1000
+N <- 50
+lse <- replicate(B, {
+  sample_n(galton_heights, N, replace = TRUE) %>%
+    lm(son ~ father, data = .) %>% .$coef
+})
+lse <- data.frame(beta_0 = lse[1,], beta_1 = lse[2,])
+
+# visual variability of estimates of coeffs
+library(gridExtra)
+p1 <- lse %>% ggplot(aes(beta_0)) + geom_histogram(binwidth = 5, color = "black")
+p2 <- lse %>% ggplot(aes(beta_1)) + geom_histogram(binwidth = 0.1, color = "black")
+grid.arrange(p1,p2, ncol = 2)
+
+# std errors are hard to calculate but can be obtained from the lm function summary
+sample_n(galton_heights, N, replace = TRUE) %>%
+  lm(son ~ father, data = .) %>% summary
+
+# compare with std errors from monte carlo sim
+lse %>% summarise(se_0 = sd(beta_0), se_1 = sd(beta_1))
--- baseball_stats.R
+++ baseball_stats.R
@@ -26,3 +26,58 @@ Teams %>% filter(yearID %in% 1961:2001) %>%
   geom_point(alpha = 0.5)
 # Shows better correlation with slope = 0.735 (although not as strong as HR vs R)
 
+# evaluate correlation between homeruns, BB and singles
+Teams %>%
+  filter(yearID %in% 1961:2001) %>%
+  mutate(Singles = (H-HR-X2B-X3B)/G, BB = BB/G, HR = HR/G) %>%
+  summarise(cor(BB,HR), cor(Singles, HR), cor(BB, Singles))
+#  0.4039125       -0.1738404      -0.05605071
+
+# to evaluate of BB is still useful for creating runs we examine the relationship
+dat <- Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(HR_strata = round(HR/G, 1),
+         BB_per_game = BB/G,
+         R_pergame = R/G) %>%
+  filter(HR_strata >= 0.4 & HR_strata <= 1.2)
+
+# make a scatter plot for each HR strata
+dat %>%
+  ggplot(aes(BB_per_game, R_pergame)) +
+  geom_point(alpha = 0.5) +
+  geom_smooth(method = "lm") +
+  facet_wrap(~HR_strata)
+
+# computing the BB slopes for each home run strata 
+dat %>%
+  group_by(HR_strata) %>%
+  summarise(slope = cor(BB_per_game, R_pergame)*sd(R_pergame)/sd(BB_per_game))
+
+
+# to evaluate of effect of HR changes when stratified against BB
+dat <- Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(BB_strata = round(BB/G, 1),
+         HR_per_game = HR/G,
+         R_pergame = R/G) %>%
+  filter(BB_strata >= 2.8 & BB_strata <= 3.9)
+
+# make a scatter plot for each BB strata
+dat %>%
+  ggplot(aes(HR_per_game, R_pergame)) +
+  geom_point(alpha = 0.5) +
+  geom_smooth(method = "lm") +
+  facet_wrap(~BB_strata)
+
+# computing the BB slopes for each home run strata 
+dat %>%
+  group_by(BB_strata) %>%
+  summarise(slope = cor(HR_per_game, R_pergame)*sd(R_pergame)/sd(HR_per_game))
+
+
+
+
+
+
+
+
+
+
